72ad765cb6604c58ca086bb842ac0476d149652e
Abu Jafar Saifullah Mahin <bsse1109@iit.du.ac.bd>
2023-09-25 09:43:11 +0600

    add basic product code

---
+++ src/main/java/com/bezkoder/spring/jwt/mongodb/controllers/ProductController.java
@@ -0,0 +1,29 @@
+package com.bezkoder.spring.jwt.mongodb.controllers;
+
+import com.bezkoder.spring.jwt.mongodb.models.Product;
+import com.bezkoder.spring.jwt.mongodb.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/v1")
+public class ProductController {
+    @Autowired
+    ProductService productService;
+
+    @GetMapping(value = "products")
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return new ResponseEntity<List<Product>>(productService.getAllProducts(), HttpStatus.OK) ;
+    }
+
+    @PostMapping(value = "product/new")
+    public ResponseEntity<Product> addProduct(@RequestBody Product product){
+        Product createdProduct = productService.createProduct((product));
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
+
+}
